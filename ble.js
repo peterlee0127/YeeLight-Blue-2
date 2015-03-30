5c74765a555d9e438e10e34fb5a3b080afe8f11f
peterlee0127 <peterlee0127@gmail.com>
2015-03-30 15:53:03 +0800

    update

--- ble.js
+++ ble.js
@@ -1,4 +1,5 @@
 var noble = require('noble');
+var util = require('util');
 
 var SERVICE_UUID          = 'fff0';
 var CONTROL_UUID          = 'fff1';
@@ -14,23 +15,35 @@ noble.on('stateChange', function(state) {
 
 noble.on('discover', function(peripheral) {
   var macAddress = peripheral.uuid;
-  var rss = peripheral.rssi;
+  // var rss = peripheral.rssi;
   var localName = peripheral.advertisement.localName;
-
     peripheral.connect(function(error){
         if(error){
           console.log(error);return;
         }
         console.log('Connected:', localName," Address:",macAddress);
-        peripheral.discoverServices([SERVICE_UUID,CONTROL_UUID], function(error, services) {
-        var deviceInformationService = services[0];
-        console.log('discovered device information service');
-        deviceInformationService.discoverCharacteristics(null, function(error, characteristics) {
-        console.log('discovered the following characteristics:');
-            for (var i in characteristics) {
-              console.log('  ' + i + ' uuid: ' + characteristics[i].uuid);
-            }
-          });
+        peripheral.discoverServices([SERVICE_UUID,CONTROL_UUID,EFFECT_UUID], function(error, services) {
+            var deviceInformationService = services[0];
+            deviceInformationService.discoverCharacteristics([CONTROL_UUID], function(error, characteristics) {
+
+                  for (var i in characteristics) {
+                    // console.log('  ' + i + ' uuid: ' + characteristics[i].uuid);
+                    if(characteristics[i].uuid==CONTROL_UUID){
+                      var red = 255;
+                      var green = 255;
+                      var blue = 255;
+                      var brightness = 100;
+                      var command = util.format('%d,%d,%d,%d', red, green, blue, brightness);
+                     for (var j = command.length; j < 18; j++) {
+                       command += ',';
+                     }
+                      characteristics[i].write(new Buffer(command), false, function(error) {
+                        if(error){console.log(error);return;}
+                          console.log('set alert level to mid (1)');
+                      });
+                    }
+                  }
+            });
         });
 
 
