258c262b15dcad7f8741dd511215f81b8875dae7
peterlee0127 <peterlee0127@gmail.com>
2015-03-30 16:15:09 +0800

    add random color

--- ble.js
+++ ble.js
@@ -25,27 +25,40 @@ noble.on('discover', function(peripheral) {
         peripheral.discoverServices([SERVICE_UUID,CONTROL_UUID,EFFECT_UUID], function(error, services) {
             var deviceInformationService = services[0];
             deviceInformationService.discoverCharacteristics([CONTROL_UUID], function(error, characteristics) {
+                  // for (var i in characteristics) {
+                    if(characteristics[0].uuid==CONTROL_UUID){
+                        // controlLight(characteristics[i],255,255,255,100);
+                        randomColor(characteristics[0]);
 
-                  for (var i in characteristics) {
-                    // console.log('  ' + i + ' uuid: ' + characteristics[i].uuid);
-                    if(characteristics[i].uuid==CONTROL_UUID){
-                      var red = 255;
-                      var green = 255;
-                      var blue = 255;
-                      var brightness = 100;
-                      var command = util.format('%d,%d,%d,%d', red, green, blue, brightness);
-                     for (var j = command.length; j < 18; j++) {
-                       command += ',';
-                     }
-                      characteristics[i].write(new Buffer(command), false, function(error) {
-                        if(error){console.log(error);return;}
-                          console.log('set alert level to mid (1)');
-                      });
-                    }
-                  }
+                    } // is CONTROL_UUID
+                  // }
             });
         });
-
-
     });
 });
+
+function randomColor(characteristics){
+  setTimeout(function () {
+    controlLight(characteristics,Math.floor((Math.random() * 255) + 1),Math.floor((Math.random() * 255) + 1),Math.floor((Math.random() * 255) + 1),100);
+    randomColor(characteristics);
+  }, 200);
+}
+
+function turnOff(characteristics){
+  controlLight(characteristics,0,0,0,0);
+}
+
+function turnOn(characteristics){
+  controlLight(characteristics,255,255,255,100);
+}
+
+function controlLight(characteristics,red,green,blue,brightness){
+  var command = util.format('%d,%d,%d,%d', red, green, blue, brightness);
+   for (var j = command.length; j < 18; j++) {
+     command += ',';
+   }
+  characteristics.write(new Buffer(command), false, function(error) {
+    if(error){console.log(error);return;}
+      // console.log('successful');
+  });
+}
